8e7a002dc09e206e9f789390ea2b3c57335395e3
L0DESTAR7 <godhand2006@hotmail.fr>
2023-04-11 06:50:35 +0000

    Clean Resert

--- API/index.ts
+++
@@ -1,9 +0,0 @@
-import { initTRPC } from '@trpc/server';
-
-const t = initTRPC.create();
-
-
-const router = t.router;
-const publicProcedute = t.procedure;
-
-const appRouter = router({}); // CONTINUE CODE
