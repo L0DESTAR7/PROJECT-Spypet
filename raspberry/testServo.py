9e35485c3102a4d3c7b189bb15d949354ab85d80
lamzaid <zaidlamrani15@gmail.com>
2023-05-21 23:13:38 +0100

    CC23-34 Improved Servomoteur test

--- raspberry/testServo.py
+++ raspberry/testServo.py
@@ -4,18 +4,19 @@ from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 factory = PiGPIOFactory()
-
-servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000,pin_factory=factory)
-
-for i in range(10):
-    if(i % 2 == 0):
-        servo.min()
-        sleep(2)
-    else:
-        servo.max()
-        sleep(2)
-
-print("Done! Going back to middle")
-servo.mid()
-sleep(2)
-servo.value = None
+try:
+    servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000,pin_factory=factory)
+    for i in range(10):
+        if(i % 2 == 0):
+            servo.min()
+            sleep(2)
+        else:
+            servo.max()
+            sleep(2)
+    print("Done! Going back to middle")
+    servo.mid()
+    sleep(2)
+    servo.value = None
+    print("Test : SERVOMOTEUR -> PASSED")
+except Exception:
+    print("Test : SERVOMOTEUR -> FAILED")
