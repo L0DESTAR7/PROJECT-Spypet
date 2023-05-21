2413dc363b1b5f12c692da5c2513706017592a3d
lamzaid <zaidlamrani15@gmail.com>
2023-05-21 22:39:53 +0100

    CC23-22 Added Load Cell 1 Tests

---
+++ raspberry/loadcell_zaid/hx711/src/GetOneWeight.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "../include/common.h"
+
+int main(int argc, char** argv) {
+
+    using namespace std;
+    using namespace HX711;
+
+    const char* const err = "Usage: [DATA PIN] [CLOCK PIN] [REFERENCE UNIT] [OFFSET]";
+
+    if(argc != 5) {
+        cerr << err << endl;
+        return EXIT_FAILURE;
+    }
+
+    const int dataPin = stoi(argv[1]);
+    const int clockPin = stoi(argv[2]);
+    const int refUnit = stoi(argv[3]);
+    const int offset = stoi(argv[4]);
+
+    SimpleHX711 hx(dataPin, clockPin, refUnit, offset);
+    const Mass m = hx.weight(3);
+    std::cout << m.getValue()<< std::endl;
+    return EXIT_SUCCESS;
+
+}
